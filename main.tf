58002ecaf7d2a35cd3e8c020fd202d9aa3531862
Jay Laws <135144614+cloudnoobs@users.noreply.github.com>
2023-06-12 13:19:50 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "app-service-demo-nc"
+  location = "East US"
+}
+
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "myappservice-demo-node"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "app_service" {
+  name                = "cloudnoobs-demo"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+}
